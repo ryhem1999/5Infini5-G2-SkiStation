6949c2891844a73e12c0d6f5549b01122f743d33
Ghassen1919 <99694534+Ghassen1919@users.noreply.github.com>
2024-02-11 19:03:10 +0100

    mockito

--- src/test/java/tn/esprit/spring/testcourse.java
+++ src/test/java/tn/esprit/spring/testcourse.java
@@ -1,64 +1,60 @@
 package tn.esprit.spring;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.mock;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import tn.esprit.spring.entities.Course;
-import tn.esprit.spring.entities.Registration; // Ajout de cette ligne
 import tn.esprit.spring.entities.Support;
 import tn.esprit.spring.entities.TypeCourse;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
 public class testcourse {
 
-    private Course course;
+    @Mock
+    private Course mockCourse;
 
-    @Before
+    @BeforeEach
     public void setUp() {
-        course = new Course();
+        MockitoAnnotations.openMocks(this);
+
+        when(mockCourse.getNumCourse()).thenReturn(1L);
+        when(mockCourse.getLevel()).thenReturn(2);
+        when(mockCourse.getTypeCourse()).thenReturn(TypeCourse.COLLECTIVE_CHILDREN);
+        when(mockCourse.getSupport()).thenReturn(Support.SKI);
+        when(mockCourse.getPrice()).thenReturn(50.0f);
+        when(mockCourse.getTimeSlot()).thenReturn(10);
     }
 
     @Test
-    public void testGettersAndSetters() {
-        // Mock dependencies
-        course = Mockito.spy(new Course());
-
-        // Set values using setter methods
-        course.setNumCourse(1L);
-        course.setLevel(2);
-        course.setTypeCourse(TypeCourse.COLLECTIVE_CHILDREN);
-        course.setSupport(Support.SKI);
-        course.setPrice(50.0f);
-        course.setTimeSlot(10);
-
-        // Test getters
-        assertEquals(Long.valueOf(1L), course.getNumCourse());
-        assertEquals(2, course.getLevel());
-        assertEquals(TypeCourse.COLLECTIVE_CHILDREN, course.getTypeCourse());
-        assertEquals(Support.SKI, course.getSupport());
-        assertEquals(50.0f, course.getPrice(), 0.0f);
-        assertEquals(10, course.getTimeSlot());
+    void testGetNumCourseWithMock() {
+        assertEquals(1L, mockCourse.getNumCourse());
     }
 
     @Test
-    public void testRegistrations() {
-        // Test initial registrations is not null
-        assertNotNull(course.getRegistrations());
+    void testGetLevelWithMock() {
+        assertEquals(2, mockCourse.getLevel());
+    }
 
-        // Test setting and getting registrations
-        Set<Registration> registrations = new HashSet<>();
-        Registration registration1 = mock(Registration.class);
-        Registration registration2 = mock(Registration.class);
-        registrations.add(registration1);
-        registrations.add(registration2);
+    @Test
+    void testGetTypeCourseWithMock() {
+        assertEquals(TypeCourse.COLLECTIVE_CHILDREN, mockCourse.getTypeCourse());
+    }
 
-        course.setRegistrations(registrations);
+    @Test
+    void testGetSupportWithMock() {
+        assertEquals(Support.SKI, mockCourse.getSupport());
+    }
 
-        assertEquals(registrations, course.getRegistrations());
+    @Test
+    void testGetPriceWithMock() {
+        assertEquals(50.0f, mockCourse.getPrice());
+    }
+
+    @Test
+    void testGetTimeSlotWithMock() {
+        assertEquals(10, mockCourse.getTimeSlot());
     }
 }
